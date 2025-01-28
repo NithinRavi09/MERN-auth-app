9faf148e38148df388d6f52de69b964873c6ffd5
Nithin Ravi <nithinravi2bcommb@gmail.com>
2025-01-28 16:03:19 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
+dotenv.config();
+
+mongoose.connect(process.env.MONGODB_URL)
+.then(() => {
+    console.log("Connected to MongoDB");
+    
+}).catch((error) => {
+    console.log(error);
+})
+
+const app = express();
+
+app.use(express.json());
+
+app.listen(3000, ()=>{
+    console.log('server listening on prot 3000');
+});
+
+app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes)
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500; // Default to 500 if statusCode isn't set
+    const message = err.message || 'Internal Server Error'; // Default error message
+    res.status(statusCode).json({ success: false, message }); // Always send JSON response
+});
